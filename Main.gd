aa240b88471b617f5bee4912cc15061504e6a1c5
RaphiMuehlbacher <raphi.muehlbacher@gmail.com>
2026-04-14 18:59:38 +0200

    implement loading first level

---
+++ Main.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+@onready var scene_root: Node = %SceneRoot;
+var current_scene: Node = null;
+
+func _ready() -> void:
+	show_menu();
+
+func show_menu() -> void:
+	change_scene("res://scenes/maps/Map_01.tscn");
+
+
+func change_scene(path: String) -> void:
+	if current_scene: 
+		current_scene.queue_free();
+	
+	current_scene = load(path).instantiate();
+	scene_root.add_child(current_scene);
