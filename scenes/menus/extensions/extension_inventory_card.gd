5c28126ced0b8a19619ae1202e0794e8646fb834
Raphael Mühlbacher <raphi.muehlbacher@gmail.com>
2026-06-08 11:45:50 +0200

    add color swatch preview to extension inventory cards

--- scenes/menus/extensions/extension_inventory_card.gd
+++ scenes/menus/extensions/extension_inventory_card.gd
@@ -8,6 +8,7 @@ var _item: WeaponExtensionItem = null
 @onready var _name_label: Label = %NameLabel
 @onready var _slot_label: Label = %SlotLabel
 @onready var _condition_label: Label = %ConditionLabel
+@onready var _color_swatch: ColorRect = %ColorSwatch
 
 
 func setup(item: WeaponExtensionItem) -> void:
@@ -39,6 +40,8 @@ func _refresh() -> void:
 		_name_label.text = "Empty"
 		_slot_label.text = ""
 		_condition_label.text = ""
+		if _color_swatch != null:
+			_color_swatch.color = Color(0.3, 0.3, 0.3, 0.5)
 		return
 
 	var tier_color: Color = _item.get_condition_color()
@@ -47,3 +50,5 @@ func _refresh() -> void:
 	_slot_label.text = _item.get_slot_display_name().to_upper()
 	_condition_label.text = "%s  %.1f" % [_item.get_condition_tier_name(), _item.condition]
 	_condition_label.add_theme_color_override("font_color", tier_color)
+	if _color_swatch != null and _item.definition != null:
+		_color_swatch.color = _item.definition.icon_color
